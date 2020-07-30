2b7a5b188239c31dc956ac9b02af220163e12e9b
paulphoku <paulphoku@gmail.com>
2020-07-30 19:07:59 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { OneSignalService } from 'ngx-onesignal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'anguonesignal';
+  constructor(public readonly onesignal: OneSignalService) {
+    // tslint:disable-next-line:no-angle-bracket-type-assertion
+    (<any> window).ngxOnesignal = onesignal;
+  }
+
+  onSubscribe() {
+    this.onesignal.subscribe();
+  }
+
+  onUnSubscribe() {
+    this.onesignal.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgxOneSignalModule } from 'ngx-onesignal';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('OneSignalSDKWorker.js', { enabled: environment.production }),
+    NgxOneSignalModule.forRoot({ appId: 'c28338f9-bdb6-4a00-93c8-882e73307d53'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
